a7530c45067d106b537854590fcc85ae66ccb424
Hannah <hmalzate@myseneca.ca>
2024-08-06 22:51:57 -0400

    update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,14 +4,16 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../App.css';
 
 const Hero = () => {
-  const images = [
-    '/banner/hero1-banner.jpg',
-    '/banner/hero2-banner.jpg',
-    '/banner/hero3-banner.jpg',
-    '/banner/baby_boss.jpg',
-    '/banner/avatar-banner.jpg',
-    '/banner/banner.jpg',
-    '/banner/avengers.jpg'
+  const media = [
+    { type: 'video', src: '/banner/Netflix\'s Chronicles of Narnia (2024) - Teaser Trailer - Greta Gerwig Reboot Concept HD.mp4' },
+    { type: 'image', src: '/banner/hero1-banner.jpg' },
+    { type: 'image', src: '/banner/hero2-banner.jpg' },
+    { type: 'image', src: '/banner/hero3-banner.jpg' },
+    { type: 'image', src: '/banner/baby_boss.jpg' },
+    { type: 'image', src: '/banner/avatar-banner.jpg' },
+    { type: 'image', src: '/banner/banner.jpg' },
+    { type: 'image', src: '/banner/avengers.jpg' }
+    
   ];
 
   return (
@@ -22,9 +24,16 @@ const Hero = () => {
       showStatus={false}
       interval={3000} // 3 seconds
     >
-      {images.map((image, index) => (
+      {media.map((item, index) => (
         <div key={index} className="hero-slide">
-          <img src={image} alt={`Slide ${index + 1}`} className="hero-img" />
+          {item.type === 'image' ? (
+            <img src={item.src} alt={`Slide ${index + 1}`} className="hero-img" />
+          ) : (
+            <video className="hero-video" controls>
+              <source src={item.src} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          )}
           <div className="hero-text">
             <h1>Welcome to Digital Movie Saga</h1>
             <p>Watch the best movies and TV shows.</p>
